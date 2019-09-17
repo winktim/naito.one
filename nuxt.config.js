7d5ac7d93033cae1e5a618c56234b7e18d67e08a
winktim <tfs.winkelmann@gmail.com>
2019-09-17 18:09:25 +0200

    Fixed purgeCSS

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,9 +65,12 @@ export default {
   purgeCSS: {
     paths: glob.sync([
       path.join(__dirname, './pages/**/*.vue'),
+      path.join(__dirname, './locales/**/*.vue'),
       path.join(__dirname, './layouts/**/*.vue'),
       path.join(__dirname, './components/**/*.vue'),
     ]),
+    // whitelist for markdown
+    whitelist: ['.nuxt-link-exact-active'],
   },
 
   /*
