a33c7705a1fadc872cd216406c5085206074fa5f
winktim <tfs.winkelmann@gmail.com>
2019-09-22 17:10:48 +0200

    Updated index

--- pages/index.vue
+++ pages/index.vue
@@ -17,19 +17,19 @@
     </section>
 
     <section class="mt-4">
-      <h2 class="font-heading text-naito-green-100 text-4xl pl-4">Services</h2>
-      <p class="pl-4">Nos services seront bientôt disponibles. Merci de votre patience.</p>
-      <div id="coming-soon-image-outer" class="relative rounded shadow h-64 mt-8 overflow-hidden">
+      <h2 class="font-heading text-naito-green-100 text-4xl pl-4">Meters</h2>
+      <p class="pl-4" v-text="$t('pages.index.services.meters.description')"></p>
+      <div id="meters-image-outer" class="relative rounded shadow h-64 mt-8 overflow-hidden">
         <nuxt-link
-          class="absolute action p-6 bg-naito-yellow-200 text-gray-900 w-2/3 sm:w-auto z-10"
-          to="/"
+          class="absolute simple-action rounded-md px-6 sm:px-8 py-4 sm:py-6 bg-naito-green-200 w-2/3 sm:w-auto z-10"
+          to="/meters"
         >
-          <span>Bientôt disponible</span>
+          <span
+            class="text-gray-100 font-bold text-center"
+            v-text="$t('pages.index.services.meters.discover')"
+          ></span>
         </nuxt-link>
-        <div
-          id="coming-soon-image"
-          class="w-full h-full pointer-events-none transition-transform-300"
-        ></div>
+        <div id="meters-image" class="w-full h-full pointer-events-none transition-transform-300"></div>
       </div>
     </section>
   </div>
@@ -64,19 +64,19 @@ export default {
 }
 </script>
 <style lang="scss">
-#coming-soon-image-outer > a {
+#meters-image-outer > a {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
 }
 
-#coming-soon-image-outer > a:hover + #coming-soon-image,
-#coming-soon-image-outer > a:active + #coming-soon-image {
-  transform: scale(1.1);
+#meters-image-outer > a:hover + #meters-image,
+#meters-image-outer > a:active + #meters-image {
+  transform: scale(1.05);
 }
 
-#coming-soon-image {
-  background-image: url(/images/coming-soon.jpg);
+#meters-image {
+  background-image: url(/images/compteur-01.jpg);
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
