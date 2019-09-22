b769d84ff0fc5dd1f4939cc4f758903af1a758a7
winktim <tfs.winkelmann@gmail.com>
2019-09-22 17:22:47 +0200

    Added redirect to wiki

---
+++ pages/meters/wiki.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <a ref="link" :href="wikiURL">Wiki</a>
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    this.$refs.link.click()
+  },
+  computed: {
+    wikiURL() {
+      return (
+        'https://github.com/winktim/meters.ch/wiki' +
+        (this.$store.state.locale === 'fr' ? '/Home-fr' : '')
+      )
+    },
+  },
+}
+</script>
