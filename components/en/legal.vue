031d7b837917db9d564308c38dcf5234e3cd598a
winktim <tfs.winkelmann@gmail.com>
2019-10-01 13:26:36 +0200

    Added legal page Added h3 style

---
+++ components/en/legal.vue
@@ -0,0 +1,60 @@
+<template>
+  <div lang="en">
+    <h1>Legal information</h1>
+    <figure>
+      <caption>Updated on 01.10.19</caption>
+      <caption>This is a faithful translation. In case of discrepancies, the French version is applied.</caption>
+    </figure>
+
+    <h2>Conditions d'utilisation</h2>
+
+    <p>The "Naito One" website is operated by the Naito One Winkelmann company.</p>
+
+    <h3>External links</h3>
+    <p>This website might include hypertext links to other websites on the Internet that are not managed by Naito One. Naito One cannot guarantee the safety and integrity of thoses websites. Visit theses sites at your own risks.</p>
+
+    <h3>Intellectual property</h3>
+    <p>Images on this website are either in the public domain, or the property of the Naito One company, except the logos of our clients, which are the property of their respective authors.</p>
+
+    <h3>No guarantee</h3>
+    <p>
+      Naito One doesn't guarantee the exactitude of the information available on this website.
+      <br />The information can be modified at any time.
+    </p>
+
+    <p>Naito One cannot be held liable for any loss or damage resulting from the use of the website or it's information.</p>
+
+    <h2>Pricavy policy</h2>
+
+    <h3>Data gathering</h3>
+    <p>
+      We believe that privacy is paramount on the Internet. That is why our data gathering policy is very straight forward.
+      <br />We do not collect any data when you browse the Naito One website.
+      <br />When you contact us by email, we gather all the information neccessary in order to handle your inquire.
+    </p>
+    <p>When you browse the Naito One website, we determine your language only using your browser's language.</p>
+
+    <h3>Cookies and web technologies</h3>
+    <p>When you browse the website, or change the display language, we store that language in your browser in order to remember it on your next visit.</p>
+    <p>We do not store any other information in your browser.</p>
+
+    <h3>Third parties</h3>
+    <p>
+      Our website loads a script from the
+      <a href="https://polyfill.io">Polyfill.io</a> service in order to ensure browser retro-compabilibity.
+    </p>
+    <p>
+      Our website is hosted on the
+      <a href="https://github.com">Github</a> servers. Because of that, they are likely to log your visits for a while.
+    </p>
+
+    <h3>Safety</h3>
+    <p>We take the security of your data and your use of the service very seriously. We follow the latest recommendations in Internet security and constantly keep our services up to date.</p>
+    <p>
+      If you have discovered a security issue you can get in touch at
+      <a
+        href="mailto:support@naito.one"
+      >support@naito.one</a>. We will take action as soon as possible.
+    </p>
+  </div>
+</template>
---
+++ components/fr/legal.vue
@@ -0,0 +1,59 @@
+<template>
+  <div lang="fr">
+    <h1>Informations légales</h1>
+    <figure>
+      <caption>Mis à jour le 01.10.19</caption>
+    </figure>
+
+    <h2>Conditions d'utilisation</h2>
+
+    <p>Le site "Naito One" est opéré par la société Naito One Winkelmann.</p>
+
+    <h3>Liens externes</h3>
+    <p>Ce site peut contenir des liens hypertexte menant à des sites Internet non gérés par Naito One. Naito One ne peut garantir leur sécurité ou leur intégrité. Visitez ces sites à vos propres risques.</p>
+
+    <h3>Propriété intellectuelle</h3>
+    <p>Les images présentes sur le site sont soit dans le domaine public, soit la propriété de l'entreprise Naito One, à l'exception des logos de nos clients, qui sont la propriété de leurs auteurs respectifs.</p>
+
+    <h3>Aucune garantie</h3>
+    <p>
+      Naito One ne garantit pas l'exactitude des informations publiées sur ce site.
+      <br />Les informations sont susceptibles d'êtres modifiées à tout moment.
+    </p>
+
+    <p>Naito One ne peut pas être tenu responsable de toute perte ou dommage résultant de l'utilisation du site web et de ses informations.</p>
+
+    <h2>Politique de confientialité</h2>
+
+    <h3>Collecte de données</h3>
+    <p>
+      Nous pensons que le respect de la vie privée est primordial sur Internet. C'est pourquoi notre politique en matière de collecte de données est très simple.
+      <br />Nous ne collectons aucune information lorsque vous naviguez sur le site web Naito One.
+      <br />Lorsque vous nous contactez par email, nous conservons toutes les informations utiles au traitement de votre demande.
+    </p>
+    <p>Lorsque vous naviguez sur le site web Naito One, nous déterminons votre langue uniquement à l'aide de la langue de votre navigateur.</p>
+
+    <h3>Cookies et technologies web</h3>
+    <p>Lorsque vous naviguez sur notre site, ou que vous modifiez la langue d'affichage, nous stockons cette langue dans votre navigateur afin de s'en souvenir lors de votre prochaine visite.</p>
+    <p>Nous ne stockons aucune autre information dans votre navigateur.</p>
+
+    <h3>Tierces</h3>
+    <p>
+      Notre site charge un script depuis le service
+      <a href="https://polyfill.io">Polyfill.io</a> afin de garantir la rétro-compabibilité des navigateurs.
+    </p>
+    <p>
+      Notre site est hébergé sur les serveurs de
+      <a href="https://github.com">Github</a>. De ce fait, ils sont susceptibles de journaliser vos visites pendant un certain temps.
+    </p>
+
+    <h3>Sécurité</h3>
+    <p>Nous prenons la sécurité de vos données et de votre utilisation du service très au sérieux. Nous suivons les dernières recommandations en terme de sécurité sur Internet et maintenons constamment nos services à jour.</p>
+    <p>
+      Si vous avez découvert un problème de sécurité vous pouvez nous contacter à
+      <a
+        href="mailto:support@naito.one"
+      >support@naito.one</a>. Nous prendrons action dans les plus brefs délais.
+    </p>
+  </div>
+</template>
