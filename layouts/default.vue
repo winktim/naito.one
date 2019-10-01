ab1ad190b304c7b28887081a6426bbaf4b28fa43
winktim <tfs.winkelmann@gmail.com>
2019-10-01 14:58:45 +0200

    IE and Edge fixes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="background-image font-content w-screen h-screen flex overflow-x-hidden">
     <sidebar></sidebar>
-    <div class="flex-grow flex flex-col md:pl-64">
+    <div class="flex-grow flex flex-col md:pl-64 w-full">
       <appbar></appbar>
       <main
         class="flex-grow main-gradient pb-24 pt-15 md:pt-0 transition-padding-top-200 text-gray-100 px-6 md:px-16 lg:px-32 xl:px-56"
