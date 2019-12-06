6ba489a736b39befea525b22e8ae8a06feaa9deb
winktim <tfs.winkelmann@gmail.com>
2019-12-06 22:35:54 +0100

    Updated legal page

--- components/fr/legal.vue
+++ components/fr/legal.vue
@@ -2,7 +2,9 @@
   <div lang="fr">
     <h1>Informations légales</h1>
     <figure>
-      <caption>Mis à jour le 01.10.19</caption>
+      <caption>
+        Mis à jour le 06.12.19
+      </caption>
     </figure>
 
     <h2>Conditions d'utilisation</h2>
@@ -10,50 +12,95 @@
     <p>Le site "Naito One" est opéré par la société Naito One Winkelmann.</p>
 
     <h3>Liens externes</h3>
-    <p>Ce site peut contenir des liens hypertexte menant à des sites Internet non gérés par Naito One. Naito One ne peut garantir leur sécurité ou leur intégrité. Visitez ces sites à vos propres risques.</p>
+    <p>
+      Ce site peut contenir des liens hypertexte menant à des sites Internet non
+      gérés par Naito One. Naito One ne peut garantir leur sécurité ou leur
+      intégrité. Visitez ces sites à vos propres risques.
+    </p>
 
     <h3>Propriété intellectuelle</h3>
-    <p>Les images présentes sur le site sont soit dans le domaine public, soit la propriété de l'entreprise Naito One, à l'exception des logos de nos clients et des images spécialement annotées, qui sont la propriété de leurs auteurs respectifs.</p>
+    <p>
+      Les images présentes sur le site sont soit dans le domaine public, soit la
+      propriété de l'entreprise Naito One, à l'exception des logos de nos
+      clients et des images spécialement annotées, qui sont la propriété de
+      leurs auteurs respectifs.
+    </p>
 
     <h3>Aucune garantie</h3>
     <p>
-      Naito One ne garantit pas l'exactitude des informations publiées sur ce site.
+      Naito One ne garantit pas l'exactitude des informations publiées sur ce
+      site.
       <br />Les informations sont susceptibles d'êtres modifiées à tout moment.
     </p>
 
-    <p>Naito One ne peut pas être tenu responsable de toute perte ou dommage résultant de l'utilisation du site web et de ses informations.</p>
+    <p>
+      Naito One ne peut pas être tenu responsable de toute perte ou dommage
+      résultant de l'utilisation du site web et de ses informations.
+    </p>
 
     <h2>Politique de confientialité</h2>
 
     <h3>Collecte de données</h3>
     <p>
-      Nous pensons que le respect de la vie privée est primordial sur Internet. C'est pourquoi notre politique en matière de collecte de données est très simple.
-      <br />Nous ne collectons aucune information lorsque vous naviguez sur le site web Naito One.
-      <br />Lorsque vous nous contactez par email, nous conservons toutes les informations utiles au traitement de votre demande.
+      Nous pensons que le respect de la vie privée est primordial sur Internet.
+      C'est pourquoi notre politique en matière de collecte de données est très
+      simple.
+      <br />Si vous activez "Do Not Track" dans votre navigateur, nous le
+      respectons et ne gardons aucune information sur vous. <br />Sinon, nous
+      gardons uniquement les informations suivantes, afin d'avoir une meilleure
+      idée de l'utilisation de notre site web.
     </p>
-    <p>Lorsque vous naviguez sur le site web Naito One, nous déterminons votre langue uniquement à l'aide de la langue de votre navigateur.</p>
+
+    <ul class="list-disc">
+      <li>Quelle page vous avez visité</li>
+      <li>
+        Votre
+        <a href="https://en.wikipedia.org/wiki/User_agent#Use_in_HTTP"
+          >Agent Utilisateur</a
+        >
+      </li>
+    </ul>
+
+    <p>
+      Lorsque vous nous contactez par email, nous conservons toutes les
+      informations utiles au traitement de votre demande.
+    </p>
+    <p>
+      Lorsque vous naviguez sur le site web Naito One, nous déterminons votre
+      langue uniquement à l'aide de la langue de votre navigateur.
+    </p>
 
     <h3>Cookies et technologies web</h3>
-    <p>Lorsque vous naviguez sur notre site, ou que vous modifiez la langue d'affichage, nous stockons cette langue dans votre navigateur afin de s'en souvenir lors de votre prochaine visite.</p>
+    <p>
+      Lorsque vous naviguez sur notre site, ou que vous modifiez la langue
+      d'affichage, nous stockons cette langue dans votre navigateur afin de s'en
+      souvenir lors de votre prochaine visite.
+    </p>
     <p>Nous ne stockons aucune autre information dans votre navigateur.</p>
 
     <h3>Tierces</h3>
     <p>
       Notre site charge un script depuis le service
-      <a href="https://polyfill.io">Polyfill.io</a> afin de garantir la rétro-compabibilité des navigateurs.
+      <a href="https://polyfill.io">Polyfill.io</a> afin de garantir la
+      rétro-compabibilité des navigateurs.
     </p>
     <p>
       Notre site est hébergé sur les serveurs de
-      <a href="https://github.com">Github</a>. De ce fait, ils sont susceptibles de journaliser vos visites pendant un certain temps.
+      <a href="https://github.com">Github</a>. De ce fait, ils sont susceptibles
+      de journaliser vos visites pendant un certain temps.
     </p>
 
     <h3>Sécurité</h3>
-    <p>Nous prenons la sécurité de vos données et de votre utilisation du service très au sérieux. Nous suivons les dernières recommandations en matière de sécurité sur Internet et maintenons constamment nos services à jour.</p>
     <p>
-      Si vous avez découvert un problème de sécurité vous pouvez nous contacter à
-      <a
-        href="mailto:support@naito.one"
-      >support@naito.one</a>. Nous prendrons action dans les plus brefs délais.
+      Nous prenons la sécurité de vos données et de votre utilisation du service
+      très au sérieux. Nous suivons les dernières recommandations en matière de
+      sécurité sur Internet et maintenons constamment nos services à jour.
+    </p>
+    <p>
+      Si vous avez découvert un problème de sécurité vous pouvez nous contacter
+      à
+      <a href="mailto:support@naito.one">support@naito.one</a>. Nous prendrons
+      action dans les plus brefs délais.
     </p>
   </div>
 </template>
